b98cd1b3000cecec1b2deef1ce91c2af39449a2b
shota <shotafujimoto90@gmail.com>
2022-01-03 15:28:45 -0800

    add

--- linked_list.py
+++ linked_list.py
@@ -73,6 +73,31 @@ class LinkedList(object):
 
         self.head = _reverse_recursive(self.head, None)
 
+    def reverse_even(self):
+        def _reverse_even(head, previous_node):
+            if head is None:
+                return None
+            current_node = head
+            while current_node and current_node.data % 2 == 0:
+                next_node = current_node.next
+                current_node.next = previous_node
+                previous_node = current_node
+                current_node = next_node
+
+            if current_node != head:
+                head.next = current_node
+                _reverse_even(current_node, None)
+                return previous_node
+            else:
+                head.next = _reverse_even(head.next, head)
+                return head
+
+        self.head = _reverse_even(self.head, None)
+
+
+
+
+
 
 
 
@@ -81,14 +106,14 @@ l.append(5)
 l.append(7)
 l.append(9)
 l.print()
-print("**************")
+print("*****append 5, 7, 9*********")
 l.insert(10)
 l.print()
-print("**************")
+print("********insert 10******")
 l.append(16)
 l.remove(9)
 l.print()
-print("**************")
+print("****append 16, remove 9**********")
 print("**************")
 l.reverse_iterative()
 l.print()
@@ -97,6 +122,19 @@ print("**************")
 print("**************")
 l.reverse_recursive()
 l.print()
+print("**************")
+l2 = LinkedList()
+l2.append(2)
+l2.append(4)
+l2.append(6)
+l2.append(1)
+l2.append(5)
+l2.append(10)
+l2.append(12)
+l2.print()
+print("*********")
+l2.reverse_even()
+l2.print()
 
 
 
